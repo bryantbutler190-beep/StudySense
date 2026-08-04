be8109c0c4ad93bbc72ebb5feb5d8b3ff0a0e448
bryantbutler190-beep <bryantbutler190@gmail.com>
2026-08-03 22:20:23 -0400

    Create main.cpp

---
+++ scr/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "=====================================\n";
+    std::cout << "      Welcome to StudySense\n";
+    std::cout << " A C++ Machine Learning Project\n";
+    std::cout << "=====================================\n";
+
+    return 0;
+}
